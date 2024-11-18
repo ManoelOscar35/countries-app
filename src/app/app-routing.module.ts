bb1316a00e064db721b866be7dd601f21004976c
ManoelOscar35 <manoeloscar557@gmail.com>
2024-11-18 18:17:12 -0300

    implementando o header e lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./components/home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: 'detail',
+    loadChildren: () => import('./components/detail/detail.module').then((m) => m.DetailModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    DetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
